69e09e68e8ecb7ff34d5176f6bc35b40990c735e
Constantin ET <emiliantivlica@yahoo.com>
2021-10-16 09:46:30 +0100

    done with templates, modified index to home page and add all blog to blog.html. Create new route for blog

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for
 
 from flask_moment import Moment
 
-
 app = Flask(__name__)
 
 moment = Moment(app)
@@ -27,12 +26,17 @@ posts = [
 
 @app.route('/')
 def home():  # ☟ Put the post in HTML page
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', title="Home")
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', posts=posts)
 
 
 if __name__ == '__main__':
