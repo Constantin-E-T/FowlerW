9918712800589a3070eabed0359c69023f5a02be
Constantin ET <emiliantivlica@yahoo.com>
2021-10-16 09:24:49 +0100

    first commit, added all files for starter project

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, url_for
+
+from flask_moment import Moment
+
+
+app = Flask(__name__)
+
+moment = Moment(app)
+
+# dummy posts
+posts = [
+    # List's
+    {
+        'author': 'Emilian Constantin',
+        'title': 'Blog Post 1',
+        'content': 'First post content!',
+        'date_posted': 'June 10, 2021'
+    },
+    {
+        'author': 'Diana Andreia',
+        'title': 'Blog Post 2',
+        'content': 'Second post content!',
+        'date_posted': 'June 12, 2021'
+    }
+]
+
+
+@app.route('/')
+def home():  # ☟ Put the post in HTML page
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run()
